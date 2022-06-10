4d7e563ce977b4ea633cb42b388cb27010c970b4
santocaridi99 <santocaridi99@gmail.com>
2022-06-10 20:12:38 +0200

    Edit completamente funzionante

--- csharp-bibliotecaMvc/Controllers/LibroesController.cs
+++ csharp-bibliotecaMvc/Controllers/LibroesController.cs
@@ -22,9 +22,9 @@ namespace csharp_bibliotecaMvc.Controllers
         // GET: Libroes
         public async Task<IActionResult> Index()
         {
-              return _context.Libri != null ? 
-                          View(await _context.Libri.ToListAsync()) :
-                          Problem("Entity set 'BibliotecaContex.Libri'  is null.");
+            return _context.Libri != null ?
+                        View(await _context.Libri.ToListAsync()) :
+                        Problem("Entity set 'BibliotecaContex.Libri'  is null.");
         }
 
         // GET: Libroes/Details/5
@@ -44,7 +44,7 @@ namespace csharp_bibliotecaMvc.Controllers
 
             var libro = await _context.Libri
                .Include(a => a.Autori)
-               .Include(p=> p.Prestiti)
+               .Include(p => p.Prestiti)
                .AsNoTracking()
                .FirstOrDefaultAsync(m => m.libroID == id);
 
@@ -66,7 +66,7 @@ namespace csharp_bibliotecaMvc.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection form)
         {
-            
+
             if (ModelState.IsValid)
             {
                 Libro nuovoLibro = new Libro();
@@ -81,8 +81,8 @@ namespace csharp_bibliotecaMvc.Controllers
                 nuovoLibro.Autori = new List<Autore>();
                 foreach (string word in words)
                 {
-                    Autore nuovoAutore = new Autore() { AutoreName = word};
-                    nuovoLibro.Autori.Add(nuovoAutore); 
+                    Autore nuovoAutore = new Autore() { AutoreName = word };
+                    nuovoLibro.Autori.Add(nuovoAutore);
                     _context.Autori.Add(nuovoAutore);
                 }
 
@@ -142,46 +142,110 @@ namespace csharp_bibliotecaMvc.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("libroID,Titolo,Descrizione,Scaffale,Stato,AutoreName")] Libro libro)
+        public IActionResult Edit(int id, Libro libro)
         {
             if (id != libro.libroID)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                try
-                {
-                    string str = Request.Form["AutoreName"];
-                    string[] words = str.Split(',');
-                    libro.Autori = new List<Autore>();
-                   
-                    foreach (string word in words)
-                    {
-                        Autore updateAutore = new Autore() { AutoreName = word };
-                        libro.Autori.Add(updateAutore);
-                        _context.Autori.Update(updateAutore);
-                    }
+                return View("Edit", libro);
 
-                    _context.Update(libro);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!LibroExists(libro.libroID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+
             }
-            return View(libro);
+
+            var aggiornaLibro = _context.Libri.Where(l => l.libroID == id).Include(a=>a.Autori).FirstOrDefault();
+            aggiornaLibro.Titolo = libro.Titolo;
+            aggiornaLibro.Scaffale = libro.Scaffale;
+            aggiornaLibro.Stato = libro.Stato;
+            aggiornaLibro.Descrizione = libro.Descrizione;
+           
+            if (libro.Autori.Count()==0)
+            {
+                aggiornaLibro.Autori = new List<Autore>();
+                string str = Request.Form["AutoreName"];
+                string[] words = str.Split(',');
+
+                foreach (string word in words)
+                {
+
+                    Autore updateAutore = new Autore() { AutoreName = word };
+                    aggiornaLibro.Autori.Add(updateAutore);
+
+                    _context.Autori.Add(updateAutore);
+                }
+            }
+            else
+            {
+                aggiornaLibro.Autori = libro.Autori;
+            }
+
+
+            //foreach(var item in libro.Autori)
+            //{
+            //    _context.Autori.RemoveRange(item);
+            //}
+
+
+           
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
+
+
+        //// POST: Libroes/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("libroID,Titolo,Descrizione,Scaffale,Stato,AutoreName")] Libro libro)
+        //{
+        //    if (id != libro.libroID)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+
+        //            string str = Request.Form["AutoreName"];
+        //            string[] words = str.Split(',');
+
+        //            //foreach(var item in libro.Autori)
+        //            //{
+        //            //    _context.Autori.RemoveRange(item);
+        //            //}
+        //            foreach (string word in words)
+        //            {
+
+        //                Autore updateAutore = new Autore() { AutoreName = word };
+        //                libro.Autori.Add(updateAutore);
+
+        //                _context.Autori.Update(updateAutore);
+        //            }
+
+        //            _context.Update(libro);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!LibroExists(libro.libroID))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(libro);
+        //}
 
         // GET: Libroes/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -215,14 +279,14 @@ namespace csharp_bibliotecaMvc.Controllers
             {
                 _context.Libri.Remove(libro);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool LibroExists(int id)
         {
-          return (_context.Libri?.Any(e => e.libroID == id)).GetValueOrDefault();
+            return (_context.Libri?.Any(e => e.libroID == id)).GetValueOrDefault();
         }
     }
 }
